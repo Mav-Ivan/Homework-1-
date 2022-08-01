43e7aa962acb4ee69d2f077f44acbfa89538d4d4
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2022-08-01 23:14:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// task 2
+
+console.log("Mavdryk")
+
+// task 3 
+
+// let a = 2
+// let b = 3
+// alert(`a = ${a}, b = ${b}`)
+// a = b
+// alert(`a = ${a}, b = ${b}`)
+
+// task 4
+
+// let obj = {
+//     "String" : "string" ,
+//     "Number" : 22 ,
+//     "Boolean" : true ,
+//     "Undefined" : undefined ,
+//     "Null" : null   
+// }
+
+// console.log(obj);
+
+// task 5 
+
+// let isAdult = confirm("Are you 18 years old or older?")
+// console.log("task 5", isAdult);
+
+// task 6
+
+// let firstName = prompt("First Name :")
+// let lastName = prompt("Last Name :")
+// let group = prompt("Your Group:")
+// let age = +prompt("What year are you born :")
+
+// console.log(firstName, lastName , group , age );
+
+// task 7 
+
+// let login = prompt("Your login : ")
+// let email = prompt("Your email : ")
+// let password = prompt("Your password : ")
+
+// alert(`Dear ${login}, your email is ${email} , your password is ${password}`)
+
+// task 8 
+
+// let hours = +prompt("Кількість годин :")
+// hours *= 3600
+// console.log(hours);
+// let days = +prompt("Кількість днів :")
+// days *= 86400
+// console.log(days);
+// let months = +prompt("Кількість місяців :")
+// months *= 2629800
+// console.log(months);
+
+// alert(`Секунд в годині : ${hours} \nСекунд в днях : ${days} \nСекунд в місяці ${months}
+// Загалом секунд : ${hours + days + months} `)
